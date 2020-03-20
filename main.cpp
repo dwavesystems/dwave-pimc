f3ec90ce77468dd86874661036ca2c6629328d22
jackraymond <10591246+jackraymond@users.noreply.github.com>
2020-03-20 13:29:53 -0700

    Tidied up preamble main.cpp

--- main.cpp
+++ main.cpp
@@ -18,37 +18,17 @@ Copyright 2020 D-Wave Systems Inc.
    Path Integral Monte Carlo code for analysis of a finite temperature transverse field Ising model,
    Defined by partition function Z = Trace[exp(-H)], and scaled Hamiltonian
    H = invTemp/2 sum_{i,j} J_{ij} \sigma^z_i \sigma^z_j + invTemp \sum_i [h_i \sigma^z_i - \Gamma\sigma^x_i]
-   //NB: J is symmetric, negative J denotes a ferromagnetic interaction
 
-   Methods exploits either single qubit Swendsen-Wang updates, or multi-qubit Swendsen-Wang updates,
+   Methods exploit either single qubit Swendsen-Wang updates, or multi-qubit Swendsen-Wang updates,
    in the latter case specifically for regular and independent 1d ferromagnetic subsequences. These
    match the methods explored in A King et al. https://arxiv.org/abs/1911.03446
 
-   Compile as: g++ -O3 -Wall -Wextra -Wno-sign-compare -std=c++11 main.cpp localPIMC.cpp -o _demo;
-   To generate projected samples following 32000 sweeps from the initial condition [Figure 17 1911.03446v2]
-   (convergence on 24 x 15 triangular cylinder, single qubit move) run the demo as:
-   ./_demo 0 IC; %IC == -1,0,1 for counterclockwise, ordered, and clockwise initial conditions
-   (convergence on 12 x 9 triangular cylinder, single qubit move) run the demo as:
-   ./_demo 1 IC; %IC == -1,0,1 for counterclockwise, ordered, and clockwise initial conditions
-   To generate projected samples following 32000 sweeps from the initial condition [Figure 15f 1911.03446v2]
-   (convergence on square octagonal lattice, 4 qubit chain moves) run the demo as:
-   ./_demo 2 IC; %IC == -1,0,1 for counterclockwise, ordered, and clockwise initial conditions
-   (convergence on square octagonal lattice, 1 qubit chain moves [faster per sweep, but slower in equilibration])
-   run the demo as:
-   ./_demo 3 IC; %IC == -1,0,1 for counterclockwise, ordered, and clockwise initial conditions
-
-   To reproduce other square octagonal lattice results call the localPIMC class lattice constructor
-   (invTempOverJ, GammaOverJ, L,qubitsPerChain=4) and algorithm (qubitsPerChain) parameters.
-   For generic models use the alternative constructor to specify the model structure.
-
-   Code is for demonstration purposes, with full functionality and some optimizations omitted. 
-
    Authors: Jack Raymond, Stephen Face
    Copyright: D-Wave Systems
-   License: Apache
+   License: Apache 2
    Last modification: March 20 2020
 
-   See also localPIMC.hpp and localPIMC.cpp
+   See also README.md localPIMC.cpp and localPIMC.hpp
 */
 
 #include <ctime>
